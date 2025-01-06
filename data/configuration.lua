ba34d4d0e0a1b8837b3a7d991389112352d242dd
Agimir <william@mesaindigo.com>
2025-01-06 13:06:59 -0600

    added options for military time / date

--- data/configuration.lua
+++ data/configuration.lua
@@ -8,3 +8,16 @@ Debug = true
 
 -- Framework options | 'ox_core', 'es_extended', 'qb-core'
 Data.System = 'ox_core'
+
+-- date & time options
+Data.Date = {
+    -- sets date to show as YYYYMMDD, false sets to MM/DD/YYYY
+    military = false,
+    -- updates base year from 2011 to 2025 on start up
+    update_year = true
+}
+
+Data.Time = {
+    -- sets time to show as 0-23, false sets to 12 hr format
+    military = false
+}
--- source/shared/support.lua
+++ source/shared/support.lua
@@ -1,12 +1,27 @@
 
 local days = {
-    sun = { int = 0, str = "Sun." },
-    mon = { int = 1, str = "Mon." },
-    tue = { int = 2, str = "Tue." },
-    wed = { int = 3, str = "Wed." },
-    thu = { int = 4, str = "Thu." },
-    fri = { int = 5, str = "Fri." },
-    sat = { int = 6, str = "Sat." }
+    sun = { int = 0,    str = "Sun." },
+    mon = { int = 1,    str = "Mon." },
+    tue = { int = 2,    str = "Tue." },
+    wed = { int = 3,    str = "Wed." },
+    thu = { int = 4,    str = "Thu." },
+    fri = { int = 5,    str = "Fri." },
+    sat = { int = 6,    str = "Sat." }
+}
+
+local months = {
+    jan = { int = 0,    str = "Jan." },
+    feb = { int = 1,    str = "Feb." },
+    mar = { int = 2,    str = "Mar." },
+    apr = { int = 3,    str = "Apr." },
+    may = { int = 4,    str = "May." },
+    jun = { int = 5,    str = "Jun." },
+    jul = { int = 6,    str = "Jul." },
+    aug = { int = 7,    str = "Aug." },
+    sep = { int = 8,    str = "Sep." },
+    oct = { int = 9,    str = "Oct." },
+    nov = { int = 10,   str = "Nov." },
+    dec = { int = 11,   str = "Dec." }
 }
 
 -- local method to return day of week
@@ -16,15 +31,31 @@ local getWeekday = function(int)
             return day.str
         end
     end
-    lib.print.error(locale('debug_errs').. "No date found from input")
+    lib.print.error(locale('debug_errs').. "No weekday found from input")
     return nil
 end
 
 -- local method to return day of week
 local getMonth = function(int)
-    
+    for _, month in pairs(months) do
+        if month.int == int then
+            return month.str
+        end
+    end
+    lib.print.error(locale('debug_errs').. "No month found from input")
+    return nil
 end
 
+-- local method for getting year
+local getYear = function(int)
+    if Data.Date.update_year then
+        return int+14
+    else
+        return  int
+    end
+end
+
+-- reference: 24hrs in GTA is 48min IRL
 -- local method for type of time display
 local timeDisplay = function(bool, int)
     if bool then
@@ -41,5 +72,12 @@ end
 
 -- function to retrieve in game date
 ReturnDate = function()
-    
+    local day = getWeekday(GetClockDayOfWeek())
+    local month = getMonth(GetClockMonth())
+    local year = getYear(GetClockYear())
+end
+
+-- test function to initial date year to 2025
+InitializeDateYear = function()
+
 end
